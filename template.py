042836e21b0169f4ae47e1788a00d87b3875ffbd
ajit kumar patel <1ajitkumarpatel@gmail.com>
2024-01-07 20:04:40 +0530

    File stracture added

---
+++ template.py
@@ -0,0 +1,41 @@
+import os                          # Accesing the operating system
+from pathlib import Path           # Handling the path object (linux/ to windows\)
+import logging                     # Logging the activity of code
+
+# Loging the information 
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+
+list_of_files = [
+    "src/__init__.py",             # This files helps to make Src as a local package
+    "src/helper.py",               # 
+    "src/prompt.py",
+    ".env",                        # This file contain all the Crediential (uid, password)
+    "setup.py",                    # This file trigger the requirements.txt and src file
+    "research/trials.ipynb",       # This file contain all the R&D notebook and files
+    "app.py",                      # 
+    "store_index.py",              #
+    "static/.gitkeep",             #
+    "templates/chat.html"          # This file contain the HTML for web app
+
+]
+
+# Creating the the upper folder stracture
+for filepath in list_of_files:
+    filepath = Path(filepath)                             # Converting into a windows path object
+    filedir, filename = os.path.split(filepath)           # Separating Folder and files prent inside the list
+
+    # If filedir is not empty then then create else noting to do. and log the info.
+    if filedir !="":                                       
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file {filename}")
+
+    # If filedir is not present or the filedir size is 0 then create the file. and log the info.
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, 'w') as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+            
+    # if the file is already exist loging the info.
+    else:
+        logging.info(f"{filename} is already created")
